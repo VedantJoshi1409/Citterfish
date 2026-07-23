c18e160118ca24c77d2f3bb72526c47bb1d72d0d
“Vedant” <vjoshi@tribalscale.com>
2026-07-23 16:36:13 -0400

    idk more changes

--- src/board.cpp
+++ src/board.cpp
@@ -1,3 +1,10 @@
-class Board {
+#include <ostream>
+#include <sstream>
+#include "board.h"
+#include <string>
 
-};
+
+std::ostream& operator<<(std::ostream& os, const Board& b) {
+
+    return os << "(" << p.x << ", " << p.y << ")";
+}
--- src/board.h
+++ src/board.h
@@ -2,6 +2,10 @@
 
 #include <cstdint>
 #include <array>
+#include <string>
+#include <ostream>
+#include <sstream>
+#include <bit>
 
 using Bitboard = std::uint64_t;
 
@@ -23,11 +27,31 @@ class Board {
     
     std::array<std::array<Bitboard, 6>, 2> pieces;
     std::array<Bitboard, 2> occupied;
-    std::array<std::array<bool, 2>, 2> castlingRights;
+    uint8_t castlingRights; // First digit is wShort castle, Second is wLong, and so on
     bool whiteToMove;
     Bitboard enPassantSquare;
     uint16_t halfmoveClock;
     uint64_t zobristHash;
     
+    Board(const std::string& fen);
+    Board();
 
+    std::array<std::array<std::string, 8>, 8> bitboardToString() {
+        std::array<std::array<std::string, 8>, 8> stringBoard;
+        for (Color color : {WHITE, BLACK}) {
+            for (Piece piece : {PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING}) {
+                Bitboard bitboard = pieces[color][piece];
+                while (bitboard) {
+                    int square = std::countr_zero(bitboard);
+                    int row = 7 - (square / 8);
+                    int col = square % 8;
+                    stringBoard[row][col] = std::string(1, pieceToChar(piece, color));
+                    bitboard &= bitboard - 1; // Clear the least significant bit
+                }
+            }
+        }
+        return stringBoard;
+    }
 };
+
+std::ostream& operator<<(std::ostream& os, const Board& b);
